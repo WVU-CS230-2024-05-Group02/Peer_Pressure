37c63eb3aa14f04b209f827ae8b793bd99f04987
ezraShowell <ezra111704@gmail.com>
2024-06-22 12:02:27 -0400

    Made compatible with React, lots of layout changes

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarButton from "./Sidebar.js";
+
+    function Sidebar(){
+        return(
+            <div className="sidebar">
+
+            </div>
+        );
+    }
+
+export default Sidebar;
---
+++ src/components/SidebarButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+    function SidebarButton(text){
+        return(
+            <div className="sidebar-button">
+                <p>The quick brown fox jumps over the lazy dog.</p>
+            </div>
+        );
+    }
+
+export default SidebarButton;
