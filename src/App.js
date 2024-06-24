d4c43caadb188aaf821ed366ebe752e6958aa83d
LucasKniska <lucaskniska1@gmail.com>
2024-06-24 00:02:34 -0400

    Student Course Page Up to Date

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Sidebar from './components/Sidebar.js';
 import SidebarButton from "./components/SidebarButton.js";
 //import HomepageContent from "./components/HomepageContent.js";
+import StudentCourse from './pages/StudentCourse.js';
 
 function App() {
   return (
     <div className="App">
-      <SidebarButton />
+      <StudentCourse/>
     </div>
   );
 }
