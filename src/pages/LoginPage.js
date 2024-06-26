83ec20a62b42cbf81ecb251944ac37c51433c68b
“JadedJewell” <saj0025@mix.wvu.edu>
2024-06-26 13:02:50 -0400

    6-26-24 Updated loggin page, implemented 50%

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Textbox from "../components/Textbox.js";
+import Footer from "../components/Footer.js";
+import '../App.js';
 
 function LoginPage(){
     return(
-        <div>
-            <img class="login-logo" src={`${process.env.PUBLIC_URL}/PeerPressureLogo.png`} alt="App logo" />
-            <Textbox email="Email:  " password="Password: " />
+        <div className="background">
+            <img className="login-logo" src={`${process.env.PUBLIC_URL}/PeerPressureLogo.png`} alt="App logo" />
+
+            <Textbox email="Email: " password="Password: "/>
+            <button>Login</button>
+            <p>If you don't already have an acount with us, please make one.</p>
+            <button>New User</button>
+            {/*<Footer />*/}
         </div>
     );
 }
