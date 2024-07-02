b030c000f678402b6e11bb200e077a843aba9068
Lcs-Knsk <lucaskniska1@gmail.com>
2024-07-02 11:07:58 -0400

    Fixed main page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
@@ -9,36 +8,27 @@ import SignupPage from "./pages/SignupPage";
 import GenericPage from "./pages/GenericPage";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AnswerEvaluation from './pages/AnswerEvaluation';
-import Alert, { notifySuccess, notifyWarning, notifyError } from './components/Alert';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from './globalStyles';
-import themes from './themes';
-import ManageStudentsPage from "./pages/ManageStudentsPage";
+import ManageStudentsPage from "./pages/ManageStudentsPage"; 
+import Alert from './components/Alert'; 
+import Navbar from './components/Navbar'; // new
+
 
 function App() {
     return (
-        <ThemeProvider theme={themes}>
-            {/* apply global styles */}
-            <GlobalStyle />
         <div className="App">
-            {/* set up routing for the application */}
             <Router>
                 <Routes>
                     <Route path="/" element={<LoginPage />} />
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/signup" element={<SignupPage />} />
-                    <Route path="/manageStudents" element={<ManageStudentsPage />} />
+                    <Route path="/evaluations" element={<AnswerEvaluation />} />
+                    <Route path="/course" element={<GenericPage><StudentCourse /></GenericPage>} />
+                    <Route path="/manageStudents" element={<ManageStudentsPage />} /> {/* Add ManageStudentsPage route */}
                     {/* add other routes here */}
                 </Routes>
             </Router>
-            {/* render the Alert component to handle notifications */}
             <Alert />
-            {/* example buttons to trigger different notifications */}
-            <button onClick={() => notifySuccess('Success!')}>Notify Success</button>
-            <button onClick={() => notifyWarning('Something is not correct!')}>Notify Warning</button>
-            <button onClick={() => notifyError('Error!')}>Notify Error</button>
         </div>
-        </ThemeProvider>
     );
 }
 
--- src/pages/GenericPage.js
+++ src/pages/GenericPage.js
@@ -9,11 +9,12 @@ import Footer from '../components/Footer.js';
 // min-vh-100 =  minimum height of the div to 100% of the viewport height
 // flex-grow-1: bootstrap class that allows the div to grow and take up the remaining space available in the flex container
 // p-3: bootstrap class that applies padding of 3 units to all sides of the div
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import Navbar from '../components/Navbar.js';
-import Sidebar from '../components/Sidebar.js';
-import Footer from '../components/Footer.js';
+
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import Navbar from '../components/Navbar.js';
+// import Sidebar from '../components/Sidebar.js';
+// import Footer from '../components/Footer.js';
 
 const GenericPage = ({ children }) => {
   return (
