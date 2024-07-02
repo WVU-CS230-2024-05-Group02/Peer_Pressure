28fde4d50475dddb129c3b5be0da84a8991a9c47
ezraShowell <ezra111704@gmail.com>
2024-07-02 12:14:06 -0400

    7-2-24 updates to navbar backend

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,22 @@
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
+import "../App.css";
+
 
 function Navbar() {
   const [click, setClick] = useState(false);
   return (
-    <>
     <nav className="navbar">
       <div className='navbar-container'>
-        <Link to='/' classNmae='navbar-logo'>
-          <img src="/PeerPressureLogo.png" alt="Peer Pressure Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
-          PEER PRESSURE <i className='fab fa-typo3' />
+        <Link to='/' className='navbar-logo'>
+          <img src="/PeerPressureLogo.png" alt="Peer Pressure Logo" />
         </Link>
         <div className='menu-icon'>
           <i className={click ? 'fas-fa-times': 'fa-solid fa-bars'} />
         </div>
       </div>
     </nav>
-    </>
   );
 };
 
