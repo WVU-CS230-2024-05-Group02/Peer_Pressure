7051ce895c04e7604c5383fde3c7dd6559fa43e5
yz0010 <yz0010@mix.wvu.edu>
2024-07-03 19:46:08 -0400

    More changes to navbar/sidebar/sidebarbutton.js and app.css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import SidebarButton from './SidebarButton';
+import '../App.css'; 
 
 const Sidebar = () => {
   return (
-    <div className="sidebar bg-light d-flex flex-column align-items-start p-3">
+    <div className="sidebar-container">
       <SidebarButton text="Sidebar Button 1" />
       <SidebarButton text="Sidebar Button 2" />
       <SidebarButton text="Sidebar Button 3" />
       <SidebarButton text="Sidebar Button 4" />
-      <button className="btn btn-danger mt-auto">Sign Out</button>
+      <div className="sidebar-signout-wrapper">
+        <button className="sidebar-signout-button">Sign Out</button>
+      </div>
     </div>
   );
 };
