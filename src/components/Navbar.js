c070e67e641c65f983e7913eea56fdb00c1e88e3
yz0010 <yz0010@mix.wvu.edu>
2024-07-03 20:23:52 -0400

    More changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,116 +1,181 @@
-// import React, {useState, useEffect} from 'react';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import { Link } from 'react-router-dom';
-// import { set } from 'react-hook-form';
-// import './Navbar.css';
-// import { Button } from './Button';
+// // import React, {useState, useEffect} from 'react';
+// // import 'bootstrap/dist/css/bootstrap.css';
+// // import { Link } from 'react-router-dom';
+// // import { set } from 'react-hook-form';
+// // import './Navbar.css';
+// // import { Button } from './Button';
 
-// function Navbar() {
-//   const [click, setClick] = useState(false);
-//   const[button, setButton] = useState(true);
+// // function Navbar() {
+// //   const [click, setClick] = useState(false);
+// //   const[button, setButton] = useState(true);
 
-//   const handleClick = () => setClick(!click);
-//   const closeMobileMenu = () => setClick(false);
+// //   const handleClick = () => setClick(!click);
+// //   const closeMobileMenu = () => setClick(false);
 
-//   const showButton = () => {
-//     if(window.innerWidth <= 960){
-//       setButton(false);
-//     } else {
-//       setButton(true);
-//     }
-//   };
+// //   const showButton = () => {
+// //     if(window.innerWidth <= 960){
+// //       setButton(false);
+// //     } else {
+// //       setButton(true);
+// //     }
+// //   };
 
-//   useEffect(() => {
-//     showButton();
-//   }, []);
+// //   useEffect(() => {
+// //     showButton();
+// //   }, []);
 
 
-//   window.addEventListener('resize', showButton);
+// //   window.addEventListener('resize', showButton);
 
-//   return (
-//     <>
-//     <nav className="navbar">
-//       <div className='navbar-container'>
-//         <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-//           <img src="/PeerPressureLogo.png" alt="Peer Pressure Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
-//           PEER PRESSURE 
-//         </Link>
-//         <div className='menu-icon' onClick={handleClick}>
-//           <i className={click ? 'fas-fa-times': 'fa-solid fa-bars'} />
-//         </div>
-//         <ul className={click ? 'nav-menu active' : 'nav-memu'}>
-//           <li className='nav-item'>
-//             <link to='/' className='nav-links' onClick={closeMobileMenu}>
-//               Button1
-//             </link>
-//           </li>
-//           <li className='nav-item'>
-//             <link to='/button2' className='nav-links' onClick={closeMobileMenu}>
-//               Button2
-//             </link>
-//           </li>
-//           <li className='nav-item'>
-//             <link to='/button3' className='nav-links' onClick={closeMobileMenu}>
-//               Button3
-//             </link>
-//           </li>
-//           <li className='nav-item'>
-//             <link to='/sign-out' className='nav-links-mobile' onClick={closeMobileMenu}>
-//               Sign Out
-//             </link>
-//           </li>
-//         </ul>
-//         {button && <Button buttonStyle='btn--outline'>SIGN OUT</Button>}
-//       </div>
-//     </nav>
-//     </>
-//   );
-// };
+// //   return (
+// //     <>
+// //     <nav className="navbar">
+// //       <div className='navbar-container'>
+// //         <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+// //           <img src="/PeerPressureLogo.png" alt="Peer Pressure Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
+// //           PEER PRESSURE 
+// //         </Link>
+// //         <div className='menu-icon' onClick={handleClick}>
+// //           <i className={click ? 'fas-fa-times': 'fa-solid fa-bars'} />
+// //         </div>
+// //         <ul className={click ? 'nav-menu active' : 'nav-memu'}>
+// //           <li className='nav-item'>
+// //             <link to='/' className='nav-links' onClick={closeMobileMenu}>
+// //               Button1
+// //             </link>
+// //           </li>
+// //           <li className='nav-item'>
+// //             <link to='/button2' className='nav-links' onClick={closeMobileMenu}>
+// //               Button2
+// //             </link>
+// //           </li>
+// //           <li className='nav-item'>
+// //             <link to='/button3' className='nav-links' onClick={closeMobileMenu}>
+// //               Button3
+// //             </link>
+// //           </li>
+// //           <li className='nav-item'>
+// //             <link to='/sign-out' className='nav-links-mobile' onClick={closeMobileMenu}>
+// //               Sign Out
+// //             </link>
+// //           </li>
+// //         </ul>
+// //         {button && <Button buttonStyle='btn--outline'>SIGN OUT</Button>}
+// //       </div>
+// //     </nav>
+// //     </>
+// //   );
+// // };
 
-// export default Navbar;
+// // export default Navbar;
 
-// import React, {useState} from 'react';
+// // import React, {useState} from 'react';
+// // import 'bootstrap/dist/css/bootstrap.css';
+// // import { Link } from 'react-router-dom';
+// // import "../App.css";
+
+
+// import React, { useState } from 'react';
 // import 'bootstrap/dist/css/bootstrap.css';
 // import { Link } from 'react-router-dom';
 // import "../App.css";
 
+// function Navbar() {
+//   const [click, setClick] = useState(false);
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <div className='container-fluid'>
+//         <Link to='/' className='navbar-brand'>
+//           <img src="/PeerPressureLogo.png" alt="Peer Pressure Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
+//           PEER PRESSURE
+//         </Link>
+//         <button className="navbar-toggler" type="button" onClick={() => setClick(!click)}>
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className={`collapse navbar-collapse ${click ? 'show' : ''}`}>
+//           <ul className="navbar-nav ml-auto">
+//             <li className="nav-item">
+//               <Link to="/home" className="nav-link">Home</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="/alert" className="nav-link">Alerts</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="/grades" className="nav-link">Grades</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="/course" className="nav-link">Course</Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
 
-import React, { useState } from 'react';
+// export default Navbar;
+
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
-import "../App.css";
+import '../App.css'; 
 
 function Navbar() {
   const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setClick(false);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className='container-fluid'>
-        <Link to='/' className='navbar-brand'>
-          <img src="/PeerPressureLogo.png" alt="Peer Pressure Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
-          PEER PRESSURE
-        </Link>
-        <button className="navbar-toggler" type="button" onClick={() => setClick(!click)}>
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${click ? 'show' : ''}`}>
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to="/home" className="nav-link">Home</Link>
+    <>
+      <nav className="navbar">
+        <div className='navbar__container'>
+          <Link to='/' className='navbar__logo' onClick={closeMobileMenu}>
+            <img src="/PeerPressureLogo.png" alt="Peer Pressure Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
+            PEER PRESSURE
+          </Link>
+          <div className='navbar__menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'navbar__menu active' : 'navbar__menu'}>
+            <li className='navbar__item'>
+              <Link to='/home' className='navbar__links' onClick={closeMobileMenu}>
+                Home
+              </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/evaluations" className="nav-link">Evaluations</Link>
+            <li className='navbar__item'>
+              <Link to='/alert' className='navbar__links' onClick={closeMobileMenu}>
+                Alerts
+              </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/grades" className="nav-link">Grades</Link>
+            <li className='navbar__item'>
+              <Link to='/grades' className='navbar__links' onClick={closeMobileMenu}>
+                Grades
+              </Link>
             </li>
-            <li className="nav-item">
-              <Link to="/course" className="nav-link">Course</Link>
+            <li className='navbar__item'>
+              <Link to='/course' className='navbar__links' onClick={closeMobileMenu}>
+                Courses
+              </Link>
             </li>
           </ul>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
 export default Navbar;
+
