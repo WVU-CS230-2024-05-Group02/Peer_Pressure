da71934cd49add43f8895aa94b4b286f6d22359e
yz0010 <yz0010@mix.wvu.edu>
2024-07-05 15:48:23 -0400

    remove comments

--- src/components/SidebarButton.js
+++ src/components/SidebarButton.js
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.css';
-
-// // accepts 'text' as a prop
-// const SidebarButton = ({ text }) => {
-//   return (
-//     // btn btn-primary btn-block mb-3 suppose to display the text prop inside the button
-//     <button className="btn btn-primary btn-block mb-3">
-//       {text}
-//     </button>
-//   );
-// };
-
-// export default SidebarButton;
-
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../App.css'; 
