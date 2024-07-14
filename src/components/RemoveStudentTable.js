2f334a8b98264ebf8c49d48b2cc21c032e34d409
banafsheh_adami <banafsheadami@gmail.com>
2024-07-14 15:24:34 -0400

    Update manage student page and add/remove student components

--- src/components/RemoveStudentTable.js
+++ src/components/RemoveStudentTable.js
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import TitleBar from './TitleBar';
 
 const RemoveStudentTable = ({ students, onRemoveSelected }) => {
-    // Used for tracking the students that are selected
     const [selectedStudents, setSelectedStudents] = useState([]);
-    //Used for paginating the page of students shown and default to the first page
     const [currentPage, setCurrentPage] = useState(1);
-    const studentsPerPage = 4; // Number of students per page
+    const studentsPerPage = 4;
 
     const handleSelect = (studentId) => {
         setSelectedStudents(prevState =>
@@ -16,72 +14,78 @@ const RemoveStudentTable = ({ students, onRemoveSelected }) => {
         );
     };
 
-    // To call the parent function to remove and update the page
     const handleRemoveSelected = () => {
         onRemoveSelected(selectedStudents);
         setSelectedStudents([]);
     };
 
-    // Get current students for the page
     const indexOfLastStudent = currentPage * studentsPerPage;
     const indexOfFirstStudent = indexOfLastStudent - studentsPerPage;
     const currentStudents = students.slice(indexOfFirstStudent, indexOfLastStudent);
 
-    // Change page of the students
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
-        <div className="col-lg-6" style={{ paddingLeft: '5%', paddingRight: '3%', flex: 1, display: 'flex', flexDirection: 'column' }}>
+        <div className="card" style={{ padding: '20px', height: '100%' }}>
             <TitleBar title="Remove Student" type="sub" />
-            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', overflow: 'hidden' }}>
                 {students.length > 0 ? (
-                    <div>
-                        <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+                    <div style={{ flex: 1, display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
+                        <table className="table table-striped" style={{ flex: '1 0 auto', marginBottom: '0' }}>
                             <thead>
                                 <tr>
                                     <th></th>
-                                    <th style={{ padding: '8px' }}>Student ID</th>
-                                    <th style={{ padding: '8px' }}>First Name</th>
-                                    <th style={{ padding: '8px' }}>Last Name</th>
-                                    <th style={{ padding: '8px' }}>Email</th>
+                                    <th style={{ width: '10%' }}>Student ID</th>
+                                    <th style={{ width: '20%' }}>First Name</th>
+                                    <th style={{ width: '20%' }}>Last Name</th>
+                                    <th style={{ width: '30%' }}>Email</th>
+                                    <th style={{ width: '20%' }}>Group</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {currentStudents.map(student => (
                                     <tr key={student.studentId}>
-                                        <td style={{ padding: '8px' }}>
+                                        <td>
                                             <input
                                                 type="checkbox"
                                                 checked={selectedStudents.includes(student.studentId)}
                                                 onChange={() => handleSelect(student.studentId)}
                                             />
                                         </td>
-                                        <td style={{ padding: '8px' }}>{student.studentId}</td>
-                                        <td style={{ padding: '8px' }}>{student.firstName}</td>
-                                        <td style={{ padding: '8px' }}>{student.lastName}</td>
-                                        <td style={{ padding: '8px' }}>{student.email}</td>
+                                        <td>{student.studentId}</td>
+                                        <td>{student.firstName}</td>
+                                        <td>{student.lastName}</td>
+                                        <td>{student.email}</td>
+                                        <td>
+                                            <select className="form-control">
+                                                <option value="">Select Group</option>
+                                                <option value="Group 1">Group 1</option>
+                                                <option value="Group 2">Group 2</option>
+                                                <option value="Group 3">Group 3</option>
+                                            </select>
+                                        </td>
                                     </tr>
                                 ))}
                             </tbody>
                         </table>
-                        <button onClick={handleRemoveSelected} style={{ marginTop: '10px', alignSelf: 'flex-start', height: '40px' }}>
-                            Remove Selected Students
-                        </button>
-                        <div style={{ marginTop: '10px', textAlign: 'center' }}>
-                            {Array.from({ length: Math.ceil(students.length / studentsPerPage) }, (_, index) => (
-                                <a
-                                    key={index + 1}
-                                    onClick={() => paginate(index + 1)}
-                                    style={{
-                                        margin: '0 5px',
-                                        cursor: 'pointer',
-                                        textDecoration: 'underline',
-                                        color: currentPage === index + 1 ? '#2c3c64' : '#000'
-                                    }}
-                                >
-                                    {index + 1}
-                                </a>
-                            ))}
+                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 0' }}>
+                            <button onClick={handleRemoveSelected} className="btn btn-danger">Remove Selected Students</button>
+                            <div className="pagination">
+                                {Array.from({ length: Math.ceil(students.length / studentsPerPage) }, (_, index) => (
+                                    <a
+                                        key={index + 1}
+                                        onClick={() => paginate(index + 1)}
+                                        style={{
+                                            margin: '0 5px',
+                                            cursor: 'pointer',
+                                            textDecoration: 'underline',
+                                            color: currentPage === index + 1 ? '#2c3c64' : '#000'
+                                        }}
+                                    >
+                                        {index + 1}
+                                    </a>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 ) : (
